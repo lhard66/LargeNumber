a4059a91c95871eb27fbc4f96c382531ebdf5024
Allen Liu <lhard@qq.com>
2019-07-11 09:20:48 +0800

    '题目'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/** 
+  * 大数据求和：
+  * 按位相加，至到结束。即个位相加、十位相加、百位相加....
+  * num1         String
+  * num2         String
+  * return       String
+**/
 export function add(num1, num2) {
   return num1 + num2;
 }
